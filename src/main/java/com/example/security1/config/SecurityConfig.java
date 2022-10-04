b50601a8a64a6e1a44f3f32b4bf09949e17cbf6b
sgb8170@naver.com <sgb8170@naver.com>
2022-10-05 02:21:21 +0900

    시큐리티 로그인 처리

--- src/main/java/com/example/security1/config/SecurityConfig.java
+++ src/main/java/com/example/security1/config/SecurityConfig.java
@@ -27,6 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/loginForm");
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login") // login 주소가 호출되면 시큐리티가 낚아채서 대신 로그인 진행
+                .defaultSuccessUrl("/");
+
     }
 }
